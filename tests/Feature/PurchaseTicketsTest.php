bb16528e71dab66549a97ab6c8c237b641e36690
KarimLa <karimacc001@gmail.com>
2021-07-18 13:24:26 +0100

    cannot purchase tickets another customer is already trying to purchase

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -13,6 +13,8 @@ class PurchaseTicketsTest extends TestCase
 {
 	use RefreshDatabase;
 
+	private FakePaymentGateway $paymentGateway;
+
 	protected function setUp(): void
 	{
 		parent::setUp();
@@ -23,7 +25,10 @@ class PurchaseTicketsTest extends TestCase
 
 	function orderTickets($concert, $params)
 	{
-		return $this->json('POST', "/concerts/{$concert->id}/orders", $params);
+		$savedRequest = $this->app['request'];
+		$response = $this->json('POST', "/concerts/{$concert->id}/orders", $params);
+		$this->app['request'] = $savedRequest;
+		return $response;
 	}
 
 	function assertValidationError($response, $field)
@@ -68,6 +73,33 @@ class PurchaseTicketsTest extends TestCase
 		$response->assertStatus(404);
 		$this->assertFalse($concert->hasOrderFor('john@example.com'));
 		$this->assertEquals(0, $this->paymentGateway->totalCharges());
+	}
+
+	function test_cannot_purchase_tickets_another_customer_is_already_trying_to_purchase()
+	{
+		$concert = Concert::factory()->published()->create(['ticket_price' => 1200])->addTickets(3);
+
+		$this->paymentGateway->beforeFirstCharge(function ($paymentGateway) use ($concert) {
+			$response =	$this->orderTickets($concert, [
+				'email' => 'personB@example.com',
+				'ticket_quantity' => 1,
+				'payment_token' => $paymentGateway->getValidTestToken(),
+			]);
+
+			$response->assertStatus(422);
+			$this->assertFalse($concert->hasOrderFor('personB@example.com'));
+			$this->assertEquals(0, $paymentGateway->totalCharges());
+		});
+
+		$this->orderTickets($concert, [
+			'email' => 'personA@example.com',
+			'ticket_quantity' => 3,
+			'payment_token' => $this->paymentGateway->getValidTestToken(),
+		]);
+
+		$this->assertEquals(3600, $this->paymentGateway->totalCharges());
+		$this->assertTrue($concert->hasOrderFor('personA@example.com'));
+		$this->assertEquals(3, $concert->ordersFor('personA@example.com')->first()->ticketQuantity());
 	}
 
 	function test_an_order_is_not_created_if_payment_fails()
